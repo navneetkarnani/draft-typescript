25c806e311e65f4828ffd5b53052dc55989c5d18
Navneet Karnani <navneet@mocsh.com>
2020-04-29 20:45:24 +0530

    Add missing tsconfig and launch.json files

--- examples/example-typescript-nodejs/index.ts
+++ examples/example-typescript-nodejs/index.ts
@@ -1,12 +1,19 @@
-import http from "http";
+import express, { RequestHandler } from "express";
+import { json } from "body-parser";
+
 const port = process.env.PORT || 3000;
 
-const requestHandler = (request, response) => {
+const requestHandler: RequestHandler = (request, response) => {
   console.log(request.url);
   response.end("Hello World, I'm Node.js!");
 };
 
-const server = http.createServer(requestHandler);
+const router = express.Router();
+router.get("/", requestHandler);
+
+const server = express();
+server.use(json());
+server.use(router);
 
 server.listen(port, () => {
   console.log(`server is listening on ${port}`);
